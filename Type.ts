32a2c00a93c9f81b04d5ba2c5a2d12d4794d188a
rlawodnr1109 <wodnr1109@naver.com>
2023-04-30 04:48:03 +0900

    feat: Type 추가

---
+++ Type.ts
@@ -0,0 +1,8 @@
+export interface TableItem {
+  id: number
+  transaction_time: string
+  status: boolean
+  customer_id: number
+  customer_name: string
+  currency: string
+}
--- src/apis/TableApi.ts/index.ts
+++ src/apis/TableApi.ts/index.ts
@@ -1,10 +1,11 @@
 import axios from 'axios'
+import { TableItem } from '../../Type'
 
 const getTradeInfo = async () => {
   const res = await axios('mock_data.json')
 
   if (res.status === 200) {
-    return res.data as any
+    return res.data as TableItem
   }
   return null
 }
