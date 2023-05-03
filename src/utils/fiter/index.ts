264ee4efee7eea9fcf0aa811f0923e7d4c2de5a0
rlawodnr1109 <wodnr1109@naver.com>
2023-05-04 07:39:56 +0900

    feat: filter 기능 구현

---
+++ src/utils/fiter/index.ts
@@ -0,0 +1,30 @@
+import { TableItem } from '../../Type'
+
+export const filterTradeByDate = (
+  initTrade: TableItem[] | undefined | null,
+  today: string
+) => {
+  if (initTrade)
+    return initTrade.filter(trade => trade.transaction_time.includes(today))
+  return []
+}
+
+export const filterTradeByCustomerName = (
+  initTrade: TableItem[],
+  name: string
+) => {
+  return initTrade.filter(trade =>
+    trade.customer_name.toUpperCase().includes(name.toUpperCase())
+  )
+}
+
+export const filterTradeByStatus = (initTrade: TableItem[], status: string) => {
+  switch (status) {
+    case 'completed':
+      return initTrade.filter(trade => trade.status === true)
+    case 'processing':
+      return initTrade.filter(trade => trade.status === false)
+    default:
+      return initTrade
+  }
+}
