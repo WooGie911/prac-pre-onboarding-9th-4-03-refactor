834d92b2a8526d180dbd1d5bc9d16c954fabf67b
rlawodnr1109 <wodnr1109@naver.com>
2023-05-04 07:30:27 +0900

    feat: React-Query 로 5초마다 데이터 받아오기 구현

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,21 @@
+import { useQuery } from 'react-query'
+import { Box, Container } from '@chakra-ui/react'
+import getTradeInfo from '@apis/TableApi'
+
+const TODAY = '2023-03-08'
+
 function Home() {
-  return <h1>Hello World</h1>
+  const { data: tradeData } = useQuery({
+    queryKey: ['getTrade'],
+    queryFn: getTradeInfo,
+    refetchInterval: 5000,
+  })
+
+  return (
+    <Container minW="1000px">
+      <Box>HOME</Box>
+    </Container>
+  )
 }
 
 export default Home
