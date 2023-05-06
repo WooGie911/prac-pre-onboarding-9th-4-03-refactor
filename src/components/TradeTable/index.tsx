3f63914c305cad7558b5b2418559a13be6798f61
rlawodnr1109 <wodnr1109@naver.com>
2023-05-06 21:09:16 +0900

    refactor: TradeTableItem 추가

--- src/components/TradeTable/index.tsx
+++ src/components/TradeTable/index.tsx
@@ -1,15 +1,34 @@
 /* eslint-disable array-callback-return */
+import { Tr, Td } from '@chakra-ui/react'
+import TradeTableItem from '../TradeTableItem'
 import { TableItem } from '../../Type'
+
+function TradeTableItems({ nowTrade }: { nowTrade: TableItem[] }) {
+  if (nowTrade.length > 0)
+    return (
+      <>
+        {nowTrade.map(item => (
+          <TradeTableItem key={item.id} nowTrade={item} />
+        ))}
+      </>
+    )
+  return (
+    <Tr>
+      <Td colSpan={6} style={{ textAlign: 'center' }}>
+        No data.
+      </Td>
+    </Tr>
+  )
+}
 
 function TradeTable(props: { trade: TableItem[] }) {
   const { trade } = props
-  return trade.map((item: TableItem) => {
-    ;<div>
-      <div>{item.id}</div>
-      <div>{item.currency}</div>
-      <div>{item.status}</div>
+
+  return (
+    <div>
+      <TradeTableItems nowTrade={trade} />
     </div>
-  })
+  )
 }
 
 export default TradeTable
---
+++ src/components/TradeTableItem/index.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable prettier/prettier */
+import { Tr, Td } from '@chakra-ui/react'
+import { TableItem } from '../../Type'
+
+interface ITradeTableItemProps {
+  nowTrade: TableItem
+}
+
+function TradeTableItem({ nowTrade }: ITradeTableItemProps) {
+  return (
+    <Tr data-testid="trade-data-list">
+      <Td data-testid="trade-data-list-id">{nowTrade.id}</Td>
+      <Td data-testid="trade-data-list-time">{nowTrade.transaction_time}</Td>
+      <Td data-testid="trade-data-list-status">
+        {nowTrade.status ? 'completed' : 'processing'}
+      </Td>
+      <Td>{nowTrade.customer_id}</Td>
+      <Td data-testid="trade-data-list-name">{nowTrade.customer_name}</Td>
+      <Td isNumeric>{nowTrade.currency}</Td>
+    </Tr>
+  )
+}
+
+export default TradeTableItem
