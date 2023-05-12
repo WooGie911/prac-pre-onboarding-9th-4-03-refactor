bfd9797a6979e177ee4c461ee9d89d304eb747f8
rlawodnr1109 <wodnr1109@naver.com>
2023-05-12 23:42:22 +0900

    feat: searchParams 적용

--- src/components/TradeTable/index.tsx
+++ src/components/TradeTable/index.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable array-callback-return */
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Tr, Td } from '@chakra-ui/react'
 import { usePagination } from 'pagination-react-js'
 import TradeTableItem from '../TradeTableItem'
 import { TableItem } from '../../Type'
 import PaginationBar from '../PaginationBar'
+import useSearchParamsURL from '../../hooks/useSearchParamsURL'
 
 function TradeTableItems({ nowTrade }: { nowTrade: TableItem[] }) {
   if (nowTrade.length > 0)
@@ -26,10 +27,16 @@ function TradeTableItems({ nowTrade }: { nowTrade: TableItem[] }) {
 
 function TradeTable(props: { trade: TableItem[] }) {
   const { trade } = props
-
   const [tradeLength, setTradeLength] = useState(trade.length)
-
   const { currentPage, entriesPerPage, entries } = usePagination(1, 50)
+
+  const { setSearchParams, getSearchParams } = useSearchParamsURL()
+
+  const { set: currentPageSet, get: currentPageGet } = currentPage
+
+  useEffect(() => {
+    setSearchParams({ page: currentPageGet.toString() })
+  }, [currentPageGet, setSearchParams])
 
   return (
     <div>
---
+++ src/hooks/useSearchParamsURL/index.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable prettier/prettier */
+import { useCallback } from 'react'
+import { useSearchParams } from 'react-router-dom'
+
+interface SearchParams {
+  [key: string]: string
+}
+
+function useSearchParamsURL() {
+  const [urlTerm, setUrlTerm] = useSearchParams()
+
+  const setSearchParams = useCallback(
+    (setParams: SearchParams) => {
+      Object.keys(setParams).forEach(key => {
+        urlTerm.set(key, setParams[key])
+        setUrlTerm(urlTerm)
+      })
+    },
+    [urlTerm, setUrlTerm]
+  )
+
+  const getSearchParams = useCallback(
+    (searchParam: string) => {
+      const urlParam = urlTerm.get(searchParam)
+      if (urlParam !== null) {
+        return urlParam
+      }
+      return ''
+    },
+    [urlTerm]
+  )
+
+  return {
+    setSearchParams,
+    getSearchParams,
+  }
+}
+
+export default useSearchParamsURL
