ab336eb50d0716c25dd12fd8d05f0dad00de4332
rlawodnr1109 <wodnr1109@naver.com>
2023-05-18 00:12:40 +0900

    refactor: styled components 사용 pagination 버튼 CSS수정

--- src/components/PaginationBar/index.tsx
+++ src/components/PaginationBar/index.tsx
@@ -1,6 +1,7 @@
-/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-use-before-define */
 import React from 'react'
 import { Pagination } from 'pagination-react-js'
+import styled from '@emotion/styled'
 
 interface IPagenationBarProps {
   entriesPerPage: {
@@ -20,36 +21,63 @@ function PaginationBar({
   currentPage,
 }: IPagenationBarProps) {
   return (
-    <Pagination
-      entriesPerPage={entriesPerPage.get}
-      totalEntries={tradeLength}
-      currentPage={{ get: currentPage.get, set: currentPage.set }}
-      offset={2}
-      classNames={{
-        wrapper: 'pagination',
-        item: 'pagination-item',
-        itemActive: 'pagination-item-active',
-        navPrev: 'pagination-item nav-item',
-        navNext: 'pagination-item nav-item',
-        navStart: 'pagination-item nav-item',
-        navEnd: 'pagination-item nav-item',
-        navPrevCustom: 'pagination-item',
-        navNextCustom: 'pagination-item',
-      }}
-      navStart="&#171;"
-      navEnd="&#187;"
-      navPrev="&#x2039;"
-      navNext="&#x203a;"
-      navPrevCustom={{
-        steps: 5,
-        content: '\u00B7\u00B7\u00B7',
-      }}
-      navNextCustom={{
-        steps: 5,
-        content: '\u00B7\u00B7\u00B7',
-      }}
-    />
+    <WrapperDiv>
+      <Pagination
+        entriesPerPage={entriesPerPage.get}
+        totalEntries={tradeLength}
+        currentPage={{ get: currentPage.get, set: currentPage.set }}
+        offset={2}
+        classNames={{
+          wrapper: 'pagination',
+          item: 'pagination-item',
+          itemActive: 'pagination-item-active',
+          navPrev: 'pagination-item nav-item',
+          navNext: 'pagination-item nav-item',
+          navStart: 'pagination-item nav-item',
+          navEnd: 'pagination-item nav-item',
+          navPrevCustom: 'pagination-item',
+          navNextCustom: 'pagination-item',
+        }}
+        navStart="&#171;"
+        navEnd="&#187;"
+        navPrev="&#x2039;"
+        navNext="&#x203a;"
+        navPrevCustom={{
+          steps: 5,
+          content: '\u00B7\u00B7\u00B7',
+        }}
+        navNextCustom={{
+          steps: 5,
+          content: '\u00B7\u00B7\u00B7',
+        }}
+      />
+    </WrapperDiv>
   )
 }
 
 export default PaginationBar
+
+const WrapperDiv = styled.div`
+  .pagination {
+    display: flex;
+    list-style: none;
+  }
+  .pagination-item {
+    padding: 5px;
+    margin: 2px;
+    border: lightgray 1px solid;
+    width: 35px;
+    text-align: center;
+    border-radius: 5px;
+  }
+
+  .pagination-item:hover {
+    cursor: pointer;
+    background-color: #e2e8f0;
+  }
+
+  .pagination-item-active {
+    color: coral;
+    font-weight: 500;
+  }
+`
